465a175599daae17f8d5944d760807c0d0939e4a
diogoofc <diogooliveira311020031999@gmail.com>
2025-08-16 17:41:36 -0300

    logica_de_programacao_java

---
+++ logica_de_programacao_java/Primeiro_projeto/src/Lógica_Sequencial.java
@@ -0,0 +1,6 @@
+public class Lógica_Sequencial {
+    public static void main(String[] args) {
+
+        System.out.println("Olá, Mundo! ");
+    }
+}
---
+++ logica_de_programacao_java/Primeiro_projeto/src/Variáveis.java
@@ -0,0 +1,7 @@
+public class Variáveis {
+    public static void main(String[] args) {
+
+
+    }
+
+}
