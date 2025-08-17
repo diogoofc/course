117b1b61fc2498b98037d82f1266122bba5c77fe
diogoofc <diogooliveira311020031999@gmail.com>
2025-08-17 10:57:28 -0300

    logica_de_programacao_java

--- logica_de_programacao_java/Primeiro_projeto/src/Variáveis.java
+++ logica_de_programacao_java/Primeiro_projeto/src/Variáveis.java
@@ -1,10 +1,15 @@
 public class Variáveis {
     public static void main(String[] args) {
 
-        int idade ;
-         idade = 22;
+        int idade = 22 ;
+        double salarioMensal = 1820.00;
+        char genero = 'M';
+        boolean empregadoJava = true;
 
-         System.out.println("Vecê tem " + idade);
+         System.out.println("Idade:" + idade);
+         System.out.println("Salario: " + salarioMensal);
+         System.out.println("Gênero:" + genero);
+         System.out.println("Empregador:" + empregadoJava);
 
     }
 
