c8fca291deeae0caa43ff47a7507e0ac00276a5b
diogoofc <diogooliveira311020031999@gmail.com>
2025-09-05 21:08:13 -0300

    curso_em_video

---
+++ curso_em_video/javaBasico/src/praticaDeCasting.java
@@ -0,0 +1,23 @@
+public class praticaDeCasting {
+    public  static void main(String[] args) {
+
+//        int nota1 = 9;
+//        int nota2 = 8;
+//        double resultado = (nota1 + nota2) / 2.0 ;
+//
+//        System.out.println(resultado);
+
+        double valorProduto = 149.75;
+        int valorEmReais = (int) valorProduto;
+
+        System.out.println(valorEmReais);
+
+
+        double idadeExtra = 25.8;
+        int anosCompletos = (int) idadeExtra;
+        System.out.println(anosCompletos);
+
+
+
+    }
+}
---
+++ curso_em_video/javaBasico/src/praticaDeEntradaDeDados.java
@@ -0,0 +1,57 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class praticaDeEntradaDeDados {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Digite seu nome completo: ");
+//        String nome = sc.nextLine();
+//
+//        System.out.println("Digite sua idade: ");
+//        int idade = sc.nextInt();
+//
+//        sc.nextLine();
+//
+//        System.out.println("Qual cidade você mora: ");
+//        String cidade = sc.nextLine();
+//
+//        System.out.printf("%s você tem %d e mora em  %s%n", nome, idade, cidade);
+//
+//
+//        sc.close();
+
+
+
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Sua Matricular: ");
+        int matricula = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Nome completo: ");
+        String nome = sc.nextLine();
+
+        System.out.println("Primeiro nota? ");
+        double nota1 = sc.nextDouble();
+        System.out.println("Segunda nota? ");
+        double nota2 = sc.nextDouble();
+
+        double media = (nota1 + nota2) / 2 ;
+
+        System.out.printf("O aluno %s, matricula %d, obteve a média %.2f%n",nome, matricula, media);
+
+        sc.close();
+
+
+
+
+
+
+
+
+    }
+}
