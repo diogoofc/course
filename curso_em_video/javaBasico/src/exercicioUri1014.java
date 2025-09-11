3e50b13c53d1fa6da2f7800b2580d382f9196ba6
diogoofc <diogooliveira311020031999@gmail.com>
2025-09-10 21:13:59 -0300

    curso_em_video

---
+++ curso_em_video/javaBasico/src/exercicioUri1014.java
@@ -0,0 +1,19 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exercicioUri1014 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int distanciaTotal = sc.nextInt();
+        double combustivoGasto = sc.nextDouble();
+
+        double consumoMedio = distanciaTotal / combustivoGasto;
+
+        System.out.printf("%.3f km/l%n", consumoMedio);
+
+        sc.close();
+
+    }
+}
