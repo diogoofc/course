32d30980102acd54de525ceadf7c737b7404ca56
diogoofc <diogooliveira311020031999@gmail.com>
2025-09-18 20:18:39 -0300

    curso_em_video

---
+++ curso_em_video/javaBasico/src/ProgramaçãoOrientadaaObjetoseAlgoritmosemJava.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+public class ProgramaçãoOrientadaaObjetoseAlgoritmosemJava {
+    public static void main (String[] args){
+
+//        char a = 'a';
+//        int b = 'b';
+//        float c = 100;
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//        System.out.println(c);
+//
+//        int d = (int) 5.1987;
+//        float e = (float) 5.0;
+//        int f = (char)  (a + 5);
+//        char g = (char) 110.5;
+//
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+//        System.out.println(g);
+
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("Digite seu nome: ");
+//        String nome = sc.nextLine();
+//
+//        System.out.println("Digite sua idade: ");
+//        int idade = sc.nextInt();
+//        System.out.println("Seu nome é " + nome + " é tem " + idade + " anos");
+
+//
+//        int a = 1;
+//        int b = 1;
+//
+//        System.out.println(++a);
+//        System.out.println(b++);
+//
+//        System.out.println(a);
+//        System.out.println(b);
+//
+//
+//        int c = 1;
+//        int d = 1;
+//
+//        System.out.println(--c);
+//        System.out.println(d--);
+//
+//        System.out.println(c);
+//        System.out.println(d);
+
+
+        int a = 3;
+        int b = 4;
+        int c = 7;
+        System.out.println((a + b) / c);
+        System.out.println(!((a > b) && (a < c)));
+
+        if(a++ >= b)
+            System.out.println(--c);
+        else
+            System.out.println(c++);
+
+        System.out.println(a % 2 == 0 ? ++a : a++);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ curso_em_video/javaBasico/src/exercicioUri1020.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class exercicioUri1020 {
+    public static  void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalDeDias = sc.nextInt();
+
+        int anos = totalDeDias / 365;
+        int diasRestantes = totalDeDias % 365;
+
+        int meses = diasRestantes / 30;
+        int dia = diasRestantes % 30;
+
+        System.out.printf("%d ano(s)%n", anos);
+        System.out.printf("%d mes(es)%n", meses);
+        System.out.printf("%d dia(s)%n",dia);
+
+        sc.close();
+
+    }
+}
