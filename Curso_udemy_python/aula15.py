0754a37512d96b85a9d654c0e691352fbf87057d
diogoofc <diogooliveira311020031999@gmail.com>
2025-10-11 16:18:29 -0300

    maratonaJava

--- Curso_udemy_python/aula15.py
+++
@@ -1,7 +0,0 @@
-# nome = input('Qual e o seu nome? ')
-# print(f'Seu nome é {nome}')
-
-numero_1 = int(input('Digite um número: '))
-numero_2 =int(input('Digite outro número: '))
-
-print(f'A soma desse número é {numero_1 + numero_2}')
--- Curso_udemy_python/aula23.py
+++
@@ -1,7 +0,0 @@
-"""
-Operador lógico "not"
-usado para inverter expressões
-not true = false
-not false = true"""
-print(not True) # false
-print(not False)# true
