5d21f4885a15be8c5cee74dd678af5cff76cef8a
diogoofc <diogooliveira311020031999@gmail.com>
2025-10-12 09:47:10 -0300

    maratonaJava

--- maratonaJava/src/academy/devdojo/maratonaJava/introducao/Aula02TiposPrimitivos.java
+++ maratonaJava/src/academy/devdojo/maratonaJava/introducao/Aula02TiposPrimitivos.java
@@ -4,19 +4,21 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, booolean
 
-        int idade = 10;
-        long numeroGrande = 100000;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
+        int idade = 22;
+        long numeroGrande = 100000L;
+        double salarioDouble = 2000.0;
+        float salarioFloat = 2500.0F;
         byte idadeByte = -128;
-        short idadeShort = 10;
+        short idadeShort = 32000;
         boolean verdadeiro = true;
         boolean falso = false;
-        char caractere = 87;
+        char caractere = '\u0041';
+        String nome = "Diogo";
 
-        System.out.println("Ele tem "+ idade + " anos");
+        System.out.println("o nome dele é " +nome + " Ele tem " + idade + " anos");
         System.out.println(verdadeiro);
         System.out.println("char "+ caractere);
+
 
     }
 }
---
+++ maratonaJava/src/academy/devdojo/maratonaJava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula03TiposPrimitivosExercicio {
+    public static void main (String[] args) {
+
+        String nome = "Diogo";
+        String endereco = "Sol Nacente";
+        double salario = 1820.0;
+        String data = "15/10/2025";
+
+        System.out.println("Eu "+ nome + ", morando no endereço "+ endereco);
+        System.out.println("confirmo que recebi o salário de " + salario + ", na data " + data);
+
+
+    }
+}
