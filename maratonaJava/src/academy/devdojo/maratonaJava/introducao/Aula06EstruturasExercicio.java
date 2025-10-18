1ecb5d60df3e5bac3a7f701346a4ad8b1759893f
diogoofc <diogooliveira311020031999@gmail.com>
2025-10-18 19:44:53 -0300

    maratonaJava

---
+++ maratonaJava/src/academy/devdojo/maratonaJava/introducao/Aula06EstruturasExercicio.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula06EstruturasExercicio {
+}
---
+++ maratonaJava/src/academy/devdojo/maratonaJava/introducao/Aula07EstruturasCondicionaisSwitchExercicio.java
@@ -0,0 +1,4 @@
+package academy.devdojo.maratonaJava.introducao;
+
+public class Aula07EstruturasCondicionaisSwitchExercicio {
+}
